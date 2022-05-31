b8c98e1b4d17b9652bb6eeb3f962a041ec64c7b2
RajKumar <KurchetisTech@gmail.com>
2022-05-31 09:48:42 +0530

    bug-10 fixed

--- src/main/java/com/test/TestApplication.java
+++ src/main/java/com/test/TestApplication.java
@@ -1,8 +1,15 @@
 package com.test;
 
 public class TestApplication {
+
 //bug-11 fix
 public String greetingMessage(String name) {
 	return "Good Morning "+name;
 }
+
+//bug-10 fix
+public void message() {
+	System.out.println("Hello");
+}
+
 }
